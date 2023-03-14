4a057c53c821c7401ee19469a925bb82926f01a0
Carolina Aliaga <carolina.aliaga-quirant-external@capgemini.com>
2023-03-14 10:31:23 +0100

    Added client route and button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthorListComponent } from './author/author-list/author-list.component';
 import { CategoryListComponent } from './category/category-list/category-list.component';
+import { ClientListComponent } from './client/client-list/client-list.component';
 import { GameListComponent } from './game/game-list/game-list.component';
 import { LoanListComponent } from './loan/loan-list/loan-list.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'categories', component: CategoryListComponent },
   { path: 'authors', component: AuthorListComponent },
   { path: 'games', component: GameListComponent},
+  { path: 'clients', component: ClientListComponent},
   { path: 'loans', component: LoanListComponent},
 ];
 
